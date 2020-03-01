47a7ed816b5f617fc7138ef2dfc753b152bd02af
Gustavo Antunes <gas.antunes84@gmail.com>
2020-03-01 10:35:14 -0300

    Implementado segurança da aplicação com spring security

--- src/main/java/br/com/gustavoantunes/fornecedor/controller/PedidoController.java
+++ src/main/java/br/com/gustavoantunes/fornecedor/controller/PedidoController.java
@@ -6,9 +6,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.gustavoantunes.fornecedor.dto.ItemDoPedidoDTO;
@@ -16,7 +16,7 @@ import br.com.gustavoantunes.fornecedor.model.Pedido;
 import br.com.gustavoantunes.fornecedor.service.PedidoService;
 
 @RestController
-@RequestMapping("pedido")
+@RequestMapping("/pedido")
 public class PedidoController {
 
 	@Autowired
@@ -25,7 +25,7 @@ public class PedidoController {
 	@Autowired
 	private static final Logger LOG = LoggerFactory.getLogger(PedidoController.class);
 	
-	@RequestMapping(method = RequestMethod.POST)
+	@PostMapping
 	public Pedido realizaPedido(@RequestBody List<ItemDoPedidoDTO> produtos) {
 		LOG.info("Pedido Recebido");
 		return pedidoService.realizaPedido(produtos);
